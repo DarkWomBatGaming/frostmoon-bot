965857af767cbdf4a093f6821785894552a53d56
DarkWomBatGaming <91429174+DarkWomBatGaming@users.noreply.github.com>
2026-04-18 02:07:20 +0545

    Simplify interactionCreate by removing command check
    Refactor interaction handling to prioritize verification.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,15 +1,9 @@
+const verify = require("../services/verificationService");
+
 module.exports = {
   name: "interactionCreate",
 
   async execute(interaction, client) {
-    const command = client.commands.get(interaction.commandName);
-
-    if (interaction.isChatInputCommand()) {
-      if (!command) return;
-      await command.execute(interaction, client);
-    }
-
-    const verify = require("../services/verificationService");
     await verify.handleInteraction(interaction, client);
   }
 };
