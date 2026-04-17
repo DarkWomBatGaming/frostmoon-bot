593f1ea525945b519ea5fd81df9908869abab063
DarkWomBatGaming <91429174+DarkWomBatGaming@users.noreply.github.com>
2026-04-18 00:44:26 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Frostmoon Bot Running"));
+
+app.listen(process.env.PORT || 3000);
 const {
   Client,
   GatewayIntentBits,
